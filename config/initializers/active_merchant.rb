61208857ea55090d0b54e60b55723e0afe6c9aeb
Jared Haworth <jared@alloycode.com>
2011-09-03 16:45:18 -0400

    Temporarily force production paypal into dev settings

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -2,8 +2,9 @@ if Rails.env.production?
   ActiveMerchant::Billing::Base.gateway_mode = :test
   ActiveMerchant::Billing::Base.integration_mode = :test
   ActiveMerchant::Billing::PaypalGateway.pem_file =
-  File.dirname(__FILE__) + '/../paypal/sandbox_paypal_cert.pem')
+  File.read(File.dirname(__FILE__) + '/../paypal/sandbox_paypal_cert.pem')
 
+  # These values are used by the ElCrypto library.
   PAYPAL_ACTION_URL   = "https://www.sandbox.paypal.com/uk/cgi-bin/webscr"
 
   PUBLIC_CERTIFICATE  = "#{Rails.root}/config/paypal/el-dev-pubcert.pem"
@@ -11,13 +12,11 @@ if Rails.env.production?
   PAYPAL_CERTIFICATE  = "#{Rails.root}/config/paypal/sandbox_paypal_cert.pem"
   PAYPAL_CERT_ID      = "92RX5FFSRK354"
   PAYPAL_USER_ACCOUNT = "elsell_1315075565_biz@gmail.com"
-
-
 else
   ActiveMerchant::Billing::Base.gateway_mode = :test
   ActiveMerchant::Billing::Base.integration_mode = :test
   ActiveMerchant::Billing::PaypalGateway.pem_file =
-  File.dirname(__FILE__) + '/../paypal/sandbox_paypal_cert.pem')
+  File.read(File.dirname(__FILE__) + '/../paypal/sandbox_paypal_cert.pem')
 
   # These values are used by the ElCrypto library.
   PAYPAL_ACTION_URL   = "https://www.sandbox.paypal.com/uk/cgi-bin/webscr"
