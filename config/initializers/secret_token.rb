583789ce24bfe5afbd7552f004ee42530598d7c1
Jared Haworth <jared@alloycode.com>
2011-10-14 10:08:40 -0400

    More secrets

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-EasternLeague::Application.config.secret_token = 'b0defc79c779ff4e999277f91254e7c328ee4536d4911c6ce078d767e5313b1dcf06bf5b3f72a30b608fcd8b00be60fcd39076f5c67e28ec387c241645809341'
