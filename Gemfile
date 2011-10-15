1cfa8f767bdfd0f90bc22b950d01f6b76d819fbf
Jared Haworth <jared@alloycode.com>
2011-10-14 23:15:33 -0400

    UPdated rails 3.1.1, added sprockets, something about a gemfile, removed redundant deploy reference to precompiling assets

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'http://rubygems.org'
 
-gem 'rails', '~> 3.1.0'
+gem 'rails', '~> 3.1.1'
 
 gem 'rake', '0.8.7'
 
@@ -10,7 +10,7 @@ group :production do
   gem 'mysql'
 end
 
-
+gem 'sprockets'
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
--- config/deploy.rb
+++ config/deploy.rb
@@ -9,29 +9,18 @@ set :scm, :git
 
 set :user, "deploy"  # The server's user for deploys
 
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
 role :web, "50.57.149.168"                          # Your HTTP server, Apache/etc
 role :app, "50.57.149.168"                          # This may be the same as your `Web` server
 role :db,  "50.57.149.168", :primary => true # This is where Rails migrations will run
 
-
 default_run_options[:pty] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
 after "deploy:update_code", "deploy:copy_configuration_files"
-after 'deploy:update_code', 'deploy:precompile_those_assets'
 
 namespace :deploy do
   task :copy_configuration_files do
     run "cp #{shared_path}/secure/*.rb #{release_path}/config/initializers"
     run "cp #{shared_path}/secure/database.yml #{release_path}/config"
-  end
-  
-  task :precompile_those_assets do
-    run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
   end
   
   task :start do ; end
