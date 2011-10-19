c7537ba189b6a10423a7eb543cbb1cf63b0f9724
Jared Haworth <jared@alloycode.com>
2011-10-19 17:29:42 -0400

    Referential link from score back to user

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -9,4 +9,12 @@ module EventsHelper
     end
   end
   
+  def score_name_and_link(score)
+    if score.competitor.pair? || score.competitor.team?
+      score.competitor.name
+    else
+      link_to score.competitor.name, user_path(score.competitor.user)
+    end
+  end
+  
 end
