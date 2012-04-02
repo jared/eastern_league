8d68b2b08156764b547f6bf56269a99e15b4fc95
Jared Haworth <jared@alloycode.com>
2012-04-02 08:50:25 -0400

    Bumped registration rate for MASKC

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -50,7 +50,7 @@ class RegistrationsController < ApplicationController
     # @flat_rate = 40.0
 
     # MASKC
-    @base_registration = 10.0
+    @base_registration = 20.0
     @discipline_rate = 10.0
     @flat_rate = 40.0
     
@@ -62,8 +62,8 @@ class RegistrationsController < ApplicationController
     end
 
     # Test against early, flat-rate fee
-    @event_registration.amount = (tmp_amount > @flat_rate) ? @flat_rate : tmp_amount
-    # @event_registration.amount = tmp_amount
+    # @event_registration.amount = (tmp_amount > @flat_rate) ? @flat_rate : tmp_amount
+    @event_registration.amount = tmp_amount
     
     if @event_registration.save
       if @event_registration.amount > 0
