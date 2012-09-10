f84bc12f13788f3af78dcafd93a68f2a96e99f2c
Jared Haworth <jared@alloycode.com>
2012-09-10 13:00:16 -0400

    Updated for multi-season support

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,8 +1,10 @@
 class CalendarsController < ApplicationController
-  
+
   def show
     @season = Season.current
     @events = @season.events.calendar
+
+    @past_seasons = Season.where(:year => 2012)
   end
-  
+
 end
