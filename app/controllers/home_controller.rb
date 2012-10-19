234fe54a0595428be94f57b21e17bab672f34446
Jared Haworth <jared@alloycode.com>
2012-10-19 13:48:07 -0400

    Flickr timeout protection

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @announcements = Announcement.recent
-    @photos = flickr.groups.pools.getPhotos(:group_id => "1888762@N20", :page => 1, :per_page => 8)
+    @photos = []
+    begin
+      @photos = flickr.groups.pools.getPhotos(:group_id => "1888762@N20", :page => 1, :per_page => 8)
+    rescue Timeout::Error
+      logger.info "Could not connect to Flickr"
+    end
   end
 
 end
