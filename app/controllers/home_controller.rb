f518737593e4cb92e0ad33611fc85625c96c949d
Jared Haworth <jared@alloycode.com>
2015-04-10 09:24:23 -0400

    Temporarily removed EL photos from homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,11 +3,11 @@ class HomeController < ApplicationController
   def index
     @announcements = Announcement.recent
     @photos = []
-    begin
-      @photos = flickr.groups.pools.getPhotos(:group_id => "1888762@N20", :page => 1, :per_page => 8)
-    rescue Timeout::Error
-      logger.info "Could not connect to Flickr"
-    end
+    # begin
+    #   @photos = flickr.groups.pools.getPhotos(:group_id => "1888762@N20", :page => 1, :per_page => 8)
+    # rescue Timeout::Error
+    #   logger.info "Could not connect to Flickr"
+    # end
   end
 
 end
