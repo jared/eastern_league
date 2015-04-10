edc9d555438f93fda5ebd7e39e02db207544fcd6
Jared Haworth <jared@alloycode.com>
2015-04-10 09:18:11 -0400

    Switched logging to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :linked_files, %w{config/database.yml config/environments/production.rb conf
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-set :linked_dirs, %w{config/paypal}
+set :linked_dirs, %w{config/paypal log}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
