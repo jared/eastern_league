1c5bf6b3f300d667ad5a1137647a8709529f83bd
Jared Haworth <jared@alloycode.com>
2015-04-10 09:13:48 -0400

    Deploy now symlinks production environment variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :repo_url, 'git@github.com:jared/eastern_league.git'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, %w{config/database.yml config/initializers/active_merchant.rb config/initializers/setup_mail.rb config/initializers/flickr.rb config/initializers/secret_token.rb}
+set :linked_files, %w{config/database.yml config/environments/production.rb config/initializers/active_merchant.rb config/initializers/setup_mail.rb config/initializers/flickr.rb config/initializers/secret_token.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
