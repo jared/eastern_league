3db909512e2e992c58b286dd5bade25624907113
Jared Haworth <jared@alloycode.com>
2015-08-02 12:26:18 -0400

    More asset wrangling

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
+set :assets_roles, [:web, :app]
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
