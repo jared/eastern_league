f05d75845a4253f68fb9b24d4766235274613c2a
Jared Haworth <jared@alloycode.com>
2015-08-02 13:52:21 -0400

    updates to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,18 +1,37 @@
 EasternLeague::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  # Code is not reloaded between requests
-  config.cache_classes = true
 
-  # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+    # Code is not reloaded between requests.
+    config.cache_classes = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_files = true
+    # Eager load code on boot. This eager loads most of Rails and
+    # your application in memory, allowing both thread web servers
+    # and those relying on copy on write to perform better.
+    # Rake tasks automatically ignore this option for performance.
+    config.eager_load = true
 
-  # Compress JavaScripts and CSS
-  config.assets.compress = true
+    # Full error reports are disabled and caching is turned on.
+    config.consider_all_requests_local       = false
+    config.action_controller.perform_caching = true
+
+    # Enable Rack::Cache to put a simple HTTP cache in front of your application
+    # Add `rack-cache` to your Gemfile before enabling this.
+    # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
+    # config.action_dispatch.rack_cache = true
+
+    # Disable Rails's static asset server (Apache or nginx will already do this).
+    config.serve_static_files = true
+
+    # Compress JavaScripts and CSS.
+    config.assets.js_compressor = :uglifier
+    # config.assets.css_compressor = :sass
+
+    # Do not fallback to assets pipeline if a precompiled asset is missed.
+    config.assets.compile = false
+
+    # Generate digests for assets URLs.
+    config.assets.digest = true
 
   # Specifies the header that your server uses for sending files
   # (comment out if your front-end server doesn't support this)
