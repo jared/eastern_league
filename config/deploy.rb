54e00d4577e7d7669f63f85e248bd25a575d0707
Jared Haworth <jared@alloycode.com>
2015-08-02 12:30:41 -0400

    more updates for asset compilation

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,17 +40,14 @@ set :linked_dirs, %w{config/paypal log tmp/pids}
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
+# namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after  :finishing,    :compile_assets
-
-end
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
+# end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,12 +2,12 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-set :assets_roles, [:web, :app]
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 server '50.57.149.168', user: 'deploy', roles: %{app db web}
+set :assets_roles, [:web, :app]
 
 
 # role-based syntax
