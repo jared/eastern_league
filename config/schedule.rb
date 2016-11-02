2241dbd479bb9cd639b557e0616a36611659bd95
Jared Haworth <jared@alloycode.com>
2016-11-02 10:07:55 -0400

    Updated cron syntax in whenever

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,11 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every '0 12 1 * *' do
+every '0 12 1,23 * *' do
   rake "membership:send_renewal_notices"
 end
-
-every '0 12 23 * *' do
-  rake "membership:send_renewal_notices"
-end
-
